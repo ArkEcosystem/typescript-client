2de8b8a642e857d4d199b8cc1bd6711b79757ee9
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2019-12-17 19:14:11 +0000

    refactor: use ky instead of got

--- src/connection.ts
+++ src/connection.ts
@@ -1,4 +1,4 @@
-import got from "got";
+import ky from "ky";
 import isUrl from "is-url-superb";
 import { RequestError } from "./errors";
 import { IResponse } from "./interfaces";
@@ -53,7 +53,7 @@ export class Connection {
 
 		try {
 			// @ts-ignore
-			const { body, headers, statusCode } = await got[method](`${this.host}/${url}`, opts);
+			const { body, headers, statusCode } = await ky[method](`${this.host}/${url}`, opts);
 
 			return {
 				body: JSON.parse(body),
