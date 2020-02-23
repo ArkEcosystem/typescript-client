cf8bf538e88f8bbb3be684e7a2724cf2141d1891
rigwild <me@rigwild.dev>
2020-02-23 19:03:01 +0100

    Add businesses typings + doc

--- src/resources/businesses.ts
+++ src/resources/businesses.ts
@@ -1,20 +1,35 @@
 import { IResponse } from "../interfaces";
+import { AllBusinessesApiQuery, BusinessBridgechainsApiQuery, SearchBusinesssApiBody, SearchBusinesssApiQuery } from "../resourcesTypes/businesses";
 import { Resource } from "./resource";
 
 export class Businesses extends Resource {
-  public async all<T = any>(query?: Record<string, any>): Promise<IResponse<T>> {
+  /**
+   * List all businesses
+   */
+  public async all<T = any>(query?: AllBusinessesApiQuery): Promise<IResponse<T>> {
     return this.sendGet("businesses", query);
   }
 
-  public async get<T = any>(id: string): Promise<IResponse<T>> {
-    return this.sendGet(`businesses/${id}`);
+  /**
+   * Return business by wallet address
+   * @param walletAddress The identifier of the wallet to be retrieved.
+   */
+  public async get<T = any>(walletAddress: string): Promise<IResponse<T>> {
+    return this.sendGet(`businesses/${walletAddress}`);
   }
 
-  public async bridgechains<T = any>(id: string, query?: Record<string, any>): Promise<IResponse<T>> {
-    return this.sendGet(`businesses/${id}/bridgechains`, query);
+  /**
+   * Return bridgechains of a business
+   * @param walletAddress The identifier of the wallet to be retrieved.
+   */
+  public async bridgechains<T = any>(walletAddress: string, query?: BusinessBridgechainsApiQuery): Promise<IResponse<T>> {
+    return this.sendGet(`businesses/${walletAddress}/bridgechains`, query);
   }
 
-  public async search<T = any>(payload?: Record<string, any>, query?: Record<string, any>): Promise<IResponse<T>> {
+  /**
+   * Search business
+   */
+  public async search<T = any>(payload?: SearchBusinesssApiBody, query?: SearchBusinesssApiQuery): Promise<IResponse<T>> {
     return this.sendPost("businesses/search", payload, query);
   }
 }
---
+++ src/resourcesTypes/businesses.ts
@@ -0,0 +1,31 @@
+import { ApiBody, ApiQuery } from "../interfaces";
+
+export interface AllBusinessesApiQuery extends ApiQuery {
+	/** Type by which it orders businesses. */
+	orderBy?: string;
+	/** The publicKey of a transaction which submitted the business. */
+	publicKey?: string;
+	/** If business is resigned or not. */
+	isResigned?: boolean;
+}
+
+export interface BusinessBridgechainsApiQuery extends ApiQuery {
+	/** Type by which it orders business. */
+	orderBy?: string;
+	/** If business is resigned or not. */
+	isResigned?: boolean;
+}
+
+export interface SearchBusinesssApiQuery extends ApiQuery {
+	/** Type by which it orders business. */
+	orderBy?: string;
+}
+export interface SearchBusinesssApiBody extends ApiBody {
+	address?: string;
+	publicKey?: string;
+	name?: string;
+	website?: string;
+	vat?: string;
+	repository?: string;
+	isResigned?: boolean;
+}
