fff9897ee78ea85d85941c685f72e9db7782c336
rigwild <me@rigwild.dev>
2020-02-23 17:50:15 +0100

    Blocks parameters typings

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,3 +3,12 @@ export interface IResponse<T> {
 	headers: { [key: string]: string };
 	status: number;
 }
+
+export interface ApiQuery extends Record<string, any> {
+	/** The number of the page that will be returned. */
+	page?: number;
+	/** The number of resources per page. */
+	limit?: number;
+}
+
+export interface ApiBody extends Record<string, any> { }
--- src/resources/blocks.ts
+++ src/resources/blocks.ts
@@ -1,8 +1,9 @@
-import { IResponse } from "../interfaces";
+import { ApiQuery, IResponse } from "../interfaces";
+import { AllBlockApiQuery, SearchAllBlockApiBody, TransactionsInBlockApiQuery } from "../resourcesTypes/blocks";
 import { Resource } from "./resource";
 
 export class Blocks extends Resource {
-	public async all<T = any>(query?: Record<string, any>): Promise<IResponse<T>> {
+	public async all<T = any>(query?: AllBlockApiQuery): Promise<IResponse<T>> {
 		return this.sendGet("blocks", query);
 	}
 
@@ -13,13 +14,16 @@ export class Blocks extends Resource {
 	public async last<T = any>(): Promise<IResponse<T>> {
 		return this.sendGet("blocks/last");
 	}
+
+	public async get<T = any>(idOrHeight: string): Promise<IResponse<T>> {
+		return this.sendGet(`blocks/${idOrHeight}`);
 	}
 
-	public async transactions<T = any>(id: string, query?: Record<string, any>): Promise<IResponse<T>> {
-		return this.sendGet(`blocks/${id}/transactions`, query);
+	public async transactions<T = any>(idOrHeight: string, query?: TransactionsInBlockApiQuery): Promise<IResponse<T>> {
+		return this.sendGet(`blocks/${idOrHeight}/transactions`, query);
 	}
 
-	public async search<T = any>(payload?: Record<string, any>, query?: Record<string, any>): Promise<IResponse<T>> {
+	public async search<T = any>(payload?: SearchAllBlockApiBody, query?: ApiQuery): Promise<IResponse<T>> {
 		return this.sendPost("blocks/search", payload, query);
 	}
 }
