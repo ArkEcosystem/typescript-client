42fc52291150f421c3bb6660c2f8bbf9f196824e
rigwild <me@rigwild.dev>
2020-02-23 18:50:33 +0100

    Add bridgechains doc

--- src/resources/bridgechains.ts
+++ src/resources/bridgechains.ts
@@ -3,14 +3,25 @@ import { AllBridgechainsApiQuery, SearchBridgechainsApiBody, SearchBridgechainsA
 import { Resource } from "./resource";
 
 export class Bridgechains extends Resource {
+  /**
+   * List all bridgechains
+   * 
+   * Returns a list of all registered bridgechains on the network.
+   */
   public async all<T = any>(query?: AllBridgechainsApiQuery): Promise<IResponse<T>> {
     return this.sendGet("bridgechains", query);
   }
 
+  /**
+   * Return bridgechain by genesis hash
+   */
   public async get<T = any>(genesisHash: string): Promise<IResponse<T>> {
     return this.sendGet(`bridgechains/${genesisHash}`);
   }
 
+  /**
+   * Search bridgechains
+   */
   public async search<T = any>(payload?: SearchBridgechainsApiBody, query?: SearchBridgechainsApiQuery): Promise<IResponse<T>> {
     return this.sendPost("bridgechains/search", payload, query);
   }
--- src/resourcesTypes/bridgechains.ts
+++ src/resourcesTypes/bridgechains.ts
@@ -2,23 +2,23 @@ import { ApiBody, ApiQuery } from "../interfaces";
 
 export interface AllBridgechainsApiQuery extends ApiQuery {
 	/** Type by which it orders bridgechains. */
-	orderBy: string;
+	orderBy?: string;
 	/** The publicKey of a transaction which submitted the bridgechain. */
-	publicKey: string;
+	publicKey?: string;
 	/** If bridgechain is resigned or not. */
-	isResigned: boolean;
+	isResigned?: boolean;
 }
 
 export interface SearchBridgechainsApiQuery extends ApiQuery {
 	/** Type by which it orders bridgechains. */
-	orderBy: string;
+	orderBy?: string;
 }
 export interface SearchBridgechainsApiBody extends ApiBody {
-	address: string;
-	publicKey: string;
-	bridgechainRepository: string;
-	genesisHash: string;
-	name: string;
-	seedNodes: string[];
-	isResigned: boolean;
+	address?: string;
+	publicKey?: string;
+	bridgechainRepository?: string;
+	genesisHash?: string;
+	name?: string;
+	seedNodes?: string[];
+	isResigned?: boolean;
 }
