7100ec3155fc1ba409167949a9106848f232c2e7
KovacZan <zan@protokol.com>
2020-10-23 09:13:08 +0200

    chore: delegates resource

--- src/resourcesTypes/delegates.ts
+++ src/resourcesTypes/delegates.ts
@@ -21,7 +21,6 @@ export interface Delegate {
 	};
 	production: {
 		approval: number;
-		productivity?: number;
 	};
 	forged: {
 		fees: number;
@@ -32,55 +31,114 @@ export interface Delegate {
 
 export interface Voter {
 	address: string;
-	publicKey: string;
-	username?: string;
-	secondPublicKey?: string;
+	publicKey?: string;
 	balance: number;
-	isDelegate: boolean;
-	vote: string;
+	nonce: number;
+	attributes: object;
 }
 
 export interface AllDelegatesApiQuery extends ApiQuery {
 	/** Type by which it orders delegates. */
 	orderBy?: string;
-	type?: "resigned" | "never-forged";
-	address?: string;
-	publicKey?: string;
-	secondPublicKey?: string;
-	vote?: string;
 	username?: string;
-	balance?: number;
-	voteBalance?: number;
-	producedBlocks?: number;
+	address?: string;
+	votes?: {
+		from?: number;
+		to?: number;
+	};
+	rank?: {
+		from?: number;
+		to?: number;
+	};
+	isResigned?: boolean;
+	blocks?: {
+		produced?: {
+			from?: number;
+			to?: number;
+		};
+		last?: {
+			id?: string;
+			height?: number;
+			timestamp: {
+				epoch: {
+					from?: number;
+					to?: number;
+				};
+				unix: {
+					from?: number;
+					to?: number;
+				};
+				human: string;
+			};
+		};
+	};
+	production: {
+		approval: {
+			from?: number;
+			to?: number;
+		};
+	};
+	forged: {
+		fees: {
+			from?: number;
+			to?: number;
+		};
+		rewards: {
+			from?: number;
+			to?: number;
+		};
+		total: {
+			from?: number;
+			to?: number;
+		};
+	};
 }
 
 export interface DelegateBlocksApiQuery extends ApiQuery {
 	/** Type by which it orders blocks. */
 	orderBy?: string;
-	id?: string;
-	version?: number;
-	timestamp?: number;
-	previousBlock?: string;
-	height?: number;
-	numberOfTransactions?: number;
-	totalAmount?: number;
-	totalFee?: number;
-	reward?: number;
-	payloadLength?: number;
-	payloadHash?: string;
-	generatorPublicKey?: string;
-	blockSignature?: string;
+	id?: string[];
+	version?: number[];
+	timestamp?: {
+		from?: number;
+		to?: number;
+	};
+	previousBlock?: string[];
+	height?: {
+		from?: number;
+		to?: number;
+	};
+	numberOfTransactions?: {
+		from?: number;
+		to?: number;
+	};
+	totalAmount?: {
+		from?: number;
+		to?: number;
+	};
+	totalFee?: {
+		from?: number;
+		to?: number;
+	};
+	reward?: {
+		from?: number;
+		to?: number;
+	};
+	payloadLength?: {
+		from?: number;
+		to?: number;
+	};
+	payloadHash?: string[];
+	generatorPublicKey?: string[];
+	blockSignature?: string[];
 }
 
 export interface DelegateVotersApiQuery extends ApiQuery {
 	/** Type by which it order voters. */
 	orderBy?: string;
-	address?: string;
-	publicKey?: string;
-	secondPublicKey?: string;
-	vote?: string;
-	username?: string;
-	balance?: number;
-	voteBalance?: number;
-	producedBlocks?: number;
+	address: string;
+	publicKey: string;
+	balance: number;
+	nonce: number;
+	attributes: object;
 }
