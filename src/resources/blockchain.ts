e2a671b410d5e0a6198051eaafc8f5a7ecbe1d35
KovacZan <zan@protokol.com>
2020-10-30 08:57:41 +0100

    fix: remove comments

--- src/resources/blockchain.ts
+++ src/resources/blockchain.ts
@@ -1,13 +1,8 @@
 import { Resource } from "./resource";
 import { ApiResponse } from "../interfaces";
 import { Blockchain as BlockchainResource } from "../resourcesTypes";
-/**
- * Retrieve Blockchain State
- */
+
 export class Blockchain extends Resource {
-	/**
-	 * Used to get the latest block and supply of the blockchain.
-	 */
 	public async index(): Promise<ApiResponse<BlockchainResource>> {
 		return this.sendGet("blockchain");
 	}
